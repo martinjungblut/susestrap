c7baa690c21620a19e7c53cba298888b4674d771
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2021-04-16 16:57:12 -0300

    Use KEYMAP=us.

--- tumbleweed.sh
+++ tumbleweed.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+set -e
+
 if [[ $EUID -ne 0 ]]; then
-   echo "You must be root to do this." 1>&2
+   echo ">>> You must be root to do this." 1>&2
    exit 1
 fi
 
 if [[ -z "$1" ]]; then
-    echo "No target directory supplied."
+    echo ">>> No target directory supplied."
     exit 2
 fi
 
 if [[ -z "$2" ]]; then
-    echo "No target hostname supplied."
+    echo ">>> No target hostname supplied."
     exit 2
 fi
 
 target=$(realpath $1)
 
 if [[ -d $target/dev ]]; then
-    echo "Target directory already contains either a partial or full installation: $target"
-    echo "Sleeping for 10 seconds... Press Ctrl+C to abort execution."
+    echo ">>> Target directory already contains either a partial or full installation: $target"
+    echo ">>> Sleeping for 10 seconds... Press Ctrl+C to abort execution."
     sleep 10
 fi
 
@@ -31,7 +33,7 @@ mount -t devpts devpts $target/dev/pts
 mount -t sysfs sysfs $target/sys
 mount -t proc proc $target/proc
 
-echo "Enabling repositories"
+echo ">>> Enabling repositories"
 zypper -R $target addrepo --refresh -p 90 --disable "http://dl.google.com/linux/chrome/rpm/stable/x86_64"      "google-chrome"
 zypper -R $target addrepo --refresh -p 90 --disable "https://download.nvidia.com/opensuse/tumbleweed"          "nvidia"
 zypper -R $target addrepo --refresh -p 90 "http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed" "packman"
@@ -40,13 +42,13 @@ zypper -R $target addrepo --refresh "http://download.opensuse.org/tumbleweed/rep
 zypper -R $target addrepo --refresh "http://download.opensuse.org/update/tumbleweed"                           "default-repo-update"
 zypper lr -Pu
 
-echo "Refreshing repositories"
+echo ">>> Refreshing repositories"
 zypper -R $target ref
 
-echo "Adding locks"
+echo ">>> Adding locks"
 zypper -R $target addlock "*yast*" "*packagekit*" "*PackageKit*" "*plymouth*" "postfix"
 
-echo "Installing base patterns"
+echo ">>> Installing base patterns"
 zypper --non-interactive -R $target install -t pattern base enhanced_base console 32bit devel_basis devel_python3 x11 basic_desktop
 
 PARAMS=(
@@ -93,14 +95,23 @@ PARAMS=(
     # google-chrome-stable
 
     ##### gaming
-    gzdoom wine wine-mono wine-gecko winetricks retroarch steam steamtricks
+    gzdoom wine wine-mono wine-gecko winetricks lutris retroarch steam steamtricks
 )
-echo "Installing base packages"
+echo ">>> Installing base packages"
 zypper --non-interactive -R $target install ${PARAMS[@]}
 
-echo "Setting up hostname, locale.conf, vconsole.conf and timezone"
+echo ">>> Setting up hostname"
 echo $2 > $target/etc/hostname
 echo "127.0.0.1 $2" >> $target/etc/hosts
+
+echo ">>> Setting up locale.conf: LANG=en_US.UTF-8"
 echo "LANG=en_US.UTF-8" > $target/etc/locale.conf
-echo "KEYMAP=br-abnt2" > $target/etc/vconsole.conf
-cd $target/etc && ln -sf ../usr/share/zoneinfo/America/Sao_Paulo localtime
+
+echo ">>> Setting up vconsole.conf: KEYMAP=us"
+echo "KEYMAP=us" > $target/etc/vconsole.conf
+
+echo ">>> It's advised that you change these settings if necessary"
+echo ">>> It's also advised for you to correctly modify /etc/fstab"
+echo ">>> It's also advised for you to set your timezone"
+echo ">>> It's also advised for you to install and configure a bootloader"
+echo ">>> Thank you for using SUSEstrap!"
